2ff26a497736430cf3820e720ad4cd9f621dd4c4
inidknqwertt <nilamdayukesuma21@gmail.com>
2025-05-10 01:25:31 +0700

    Part 3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -96,9 +96,9 @@ div.content {
                 </div>
         </div>
         <div class="col-md-9">
-            <div class="content">
+            
                  @yield('content')
-            </div>
+            
         </div>
     </div>
 
