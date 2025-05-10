855582ab4e65c3dfc4a593b186514791a8a79bb2
inidknqwertt <nilamdayukesuma21@gmail.com>
2025-05-10 10:02:01 +0700

    Part 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,5 @@ Route::get('/', function () {
 });
 
 Route::resource("/students", StudentController::class);
+
+Route::resource("/teachers", TeacherController::class);
